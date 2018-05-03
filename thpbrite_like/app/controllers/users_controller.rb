522d7cab3212e92ebdd599bd8d9a0b7fbbce8721
iMassim0 <i.max@live.fr>
2018-05-03 12:13:26 +0200

    css ok + crea user

--- thpbrite_like/app/controllers/users_controller.rb
+++ thpbrite_like/app/controllers/users_controller.rb
@@ -1,22 +1,51 @@
 class UsersController < ApplicationController
-  def index
+  def new
+    @user = User.new
   end
 
   def create
-  end
-
-  def new
+    @user = User.create(params_user)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      flash[:error] = 'Rentre bien un pseudo et un mot de passe convenable !'
+      redirect_to new_user_path
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+    # @comment = Comment.new
+    # @comments = Comment.where(gossip_id: params[:id])
   end
 
   def edit
-  end
-
-  def update
+    # @user = User.find(params[:id])
   end
 
   def destroy
+    # @user = User.find(params[:id])
+    # @user.destroy
+    # redirect_to root_path
+  end
+
+  def update
+    # @user = User.find(params[:id])
+    # @user.update(params_user)
+    # if @user.save
+    #   redirect_to gossip_path(@user.id)
+    # else
+    #   flash[:error] = 'Rentre bien un pseudo et un texte !'
+    #   redirect_to edit_gossip_path
+    # end
+  end
+
+  def index
+    
+  end
+
+  private
+  def params_user
+    params_user = params.require(:user).permit(:anonymous_author, :content)
   end
 end
